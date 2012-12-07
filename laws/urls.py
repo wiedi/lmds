82c5f661e461718a7eeaf935aabf8db30b3100a9
Sebastian Wiedenroth <wiedi@frubar.net>
2012-12-07 16:00:02 +0100

    implement meta_data call

--- laws/urls.py
+++ laws/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('laws.views',
     url(r'^$',              'api_version'),
+    url(r'^(?P<version>[^/]+)/meta-data/$',            'meta_data'),
     url(r'^(?P<version>[^/]+)/meta-data/instance-id$', 'instance_id'),
-    url(r'^(?P<version>[^/]+)/meta-data/user-data$',   'user_data'),
+    url(r'^(?P<version>[^/]+)/user-data$',             'user_data'),
 )
